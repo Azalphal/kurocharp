86b0e3ecf1e4f6580ff2ab8e0bf7cca1dd688442
Bouyou34 <bouyou-mael@live.fr>
2017-12-17 11:19:50 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const discord = require('discord.js');
+const client = new discord.Client();
+const settings = require('./settings.json');
+
+client.on('ready', () => {
+    console.log(`
+        Bot ready. Currently in:
+        ${client.channels.size} Channels,
+        ${client.guilds.size} Servers,
+        With a ${client.users.size} total user.
+        `)
+  });
+
+  const prefix = ">";
+  client.on("message", (message) => {
+    if (!message.content.indexOf(prefix) !== 0 || message.author.bot) return;
+  
+    if (message.content.startsWith(prefix + "ping")) {
+      message.channel.send(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
+    }
+});
