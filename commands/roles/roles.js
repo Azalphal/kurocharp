9bb0866188c542da0271e7416182627fc09540cb
Bouyou34 <bouyou-mael@live.fr>
2017-12-18 20:56:46 +0100

    fixed role command

--- commands/roles/roles.js
+++ commands/roles/roles.js
@@ -12,22 +12,16 @@ class roleCommand extends Command {
 
     async run(message, channel, user, args) {
         if (message.channel.id !== "392046551179984896") {
-            console.log(`${message.author.username} tried to run role command in ${channel.name}`);
+            console.log(`${message.author.username} tried to run role command in ${channel.name} but failed.`);
+            message.delete();
             console.log(args);
             return;
         }
-        var SAroles = ['red', 'green', 'yellow', 'blue', 'pink', 'purple', 'orange'];
-        let [red, green, yellow, blue, pink, purple, orange] = SAroles;
+        var SAroles = ['red', 'green', 'yellow', 'blue', 'pink', 'purple', 'orange', 'male', 'female', 'trans'];
         const roles = args;
-        console.log(args)
-        if (SAroles.isArray(roles.toLower()) > -1) {
-            message.guild.roles.forEach(role => {
-                if (role.name == roles.toLower()) {
-                    message.member.addRole(role).catch(console.error);
-                    return;
-                }
-            });
-        }
+        console.log(roles, `${message.author.username} ran role command in ${channel.name} to get role`, roles)
+        message.delete(`${message.author.username} ran role command in ${channel.name}`);
+        message.member.addRole(message.guild.roles.find("name", roles[0]))
     }
 }
 module.exports = roleCommand;
