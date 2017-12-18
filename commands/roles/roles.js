7477c915590d37f4038af27a231bdd2cc2b45ab7
Bouyou34 <bouyou-mael@live.fr>
2017-12-18 18:18:30 +0100

    made role command work

--- commands/roles/roles.js
+++ commands/roles/roles.js
@@ -5,26 +5,27 @@ class roleCommand extends Command {
     constructor(client) {
         super(client, {
             name: "role",
-            description: "Give the user a role in the <#392046551179984896> channel.",
+            description: "Give the user a role.",
             aliases: ["roles"]
         });
     }
 
     async run(message, channel, user, args) {
         if (message.channel.id !== "392046551179984896") {
-            message.delete();
+            console.log(`${message.author.username} tried to run role command in ${channel.name}`);
+            console.log(args);
             return;
         }
-
+        var SAroles = ['red', 'green', 'yellow', 'blue', 'pink', 'purple', 'orange'];
+        let [red, green, yellow, blue, pink, purple, orange] = SAroles;
         const roles = args;
         console.log(args)
-        var SAroles = new SAroles['red', 'green', 'yellow', 'blue', 'pink', 'purple', 'orange']
-        if (SAroles.indexOf(roles.toLower()) > -1) {
+        if (SAroles.isArray(roles.toLower()) > -1) {
             message.guild.roles.forEach(role => {
                 if (role.name == roles.toLower()) {
                     message.member.addRole(role).catch(console.error);
                     return;
-                }                
+                }
             });
         }
     }
