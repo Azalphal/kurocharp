48805f75aa8c91e7c69f0a562b9990ecc691d249
Bouyou34 <bouyou-mael@live.fr>
2017-12-19 19:09:59 +0100

    fix

--- commands/other/av.js
+++ commands/other/av.js
@@ -5,13 +5,16 @@ class av extends Command {
         super(client, {
             name: "av",
             description: "Show the beautiful ~~or not~~ avatar of someone.",
-            aliases: ["avatar"]
+            aliases: ["avatar", "avi"]
         });
     }
 
-    async run(client, message) {
-        let target = message.mention.users.size === 0 ? message.author : message.guild.member(message.mentions.users.first()).user;
-        message.channel.send(targer.avatarURL);
+    async run (client, channel, message) {
+        message.channel.send(message.author.avatarURL);
     }
+    /*async run(client, message) {
+        let target = message.mention.users.size === 0 ? message.author : message.guild.member(message.mentions.users.first()).user;
+        message.channel.send(target.avatarURL);
+    }*/
 }
 module.exports = av;
